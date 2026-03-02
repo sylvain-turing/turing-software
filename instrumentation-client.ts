d4a4ad382f2505b8f712c7d71eefb98d2671abcb
Toni GARCIA <toni.garcia.freelance@gmail.com>
2026-03-02 15:47:57 +0100

    fix error next

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,7 +1,10 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
+  api_host:
+    process.env.NODE_ENV === "development"
+      ? "https://eu.i.posthog.com"
+      : "/ingest",
   ui_host: "https://eu.posthog.com",
   // Include the defaults option as required by PostHog
   defaults: "2026-01-30",
